2409abbcda79788bfdfeeb26a319d62180119b12
Drei08 <andrei_rheivjarth@hotmail.com>
2025-11-05 22:03:44 -0300

    feat: created service/exception

---
+++ src/main/java/com/javanauta/aprendendospring/business/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.javanauta.aprendendospring.business;
+
+import com.javanauta.aprendendospring.infrastructure.entity.Usuario;
+import com.javanauta.aprendendospring.infrastructure.exceptions.ConflictException;
+import com.javanauta.aprendendospring.infrastructure.repository.UsuarioReposiroty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioReposiroty usuarioReposiroty;
+
+    public Usuario salvaUsuario(Usuario usuario){
+        try{
+            emailExiste(usuario.getEmail());
+            return usuarioReposiroty.save(usuario);
+
+        }catch (ConflictException e){
+            throw new ConflictException("Email ja cadastrado", e.getCause());
+
+        }
+    }
+
+    public void emailExiste(String email){
+        try{
+            boolean exist = verificaEmailExistente(email);
+            if(exist){
+                throw new ConflictException("Email já cadastrado" + email);
+            }
+
+        }catch (ConflictException e){
+            throw new ConflictException("Email já cadastrado" + e.getCause());
+        }
+    }
+
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioReposiroty.existsByEmail(email);
+    }
+}
